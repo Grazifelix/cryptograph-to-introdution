52d9cbb4460de2281ef9d61dac786b1c9d6ba5c9
Grazi Felix <grazielafelix2001@gmail.com>
2022-07-07 17:15:52 -0300

    feat: add keys verification

--- 5-affine-cipher.py
+++ 5-affine-cipher.py
@@ -1,23 +1,22 @@
-# 06/07/22
+# Inicio: 06/07/22 - fim: 07/07/22
+# Graziela Felix
 
-
-mensage = input('Mensagem: ')
-
+mensage = input('Mensage: ')
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z']
 
 # remove especials and blank characters
 def clean_text(m):
     result = ''
     for letra in m:
-        r = comparacao(letra.upper())
+        r = text_compare(letra.upper())
         if r is True:
             result = result + letra.lower()
 
     return result
 
 
-def comparacao(letra):
-    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-                'V', 'W', 'X', 'Y', 'Z', 'Á', 'É', 'Í', 'Ó', 'Ú', 'Ê', 'Ô', 'Ã', 'Õ']
+def text_compare(letra):
 
     if letra == ' ':
         return False
@@ -29,34 +28,74 @@ def comparacao(letra):
         return False
 
 
-print(clean_text(mensage))
-
-# affine cipher
 mensage_clean = clean_text(mensage)
-# p = input().split('')
-a = int(input("Chave a: ")) # need verification to the rule i ≤ b ≤ m
-b = int(input("Chave b: "))
 
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-            'V', 'W', 'X', 'Y', 'Z']
+# Key 'a' Verification to the rule 1 ≤ a ≤ m, gcd(a, m) = 1
 
-def affineCipher(acm, al):
+def key_a_verification():
+    a = int(input("Chave a: "))
+    divisor = 0
+    dividendo = 0
+    m = len(alphabet)
+
+    if (a < m):
+        dividendo = a
+        divisor = m
+    else:
+        dividendo = m
+        divisor = a
+
+    # GCD verification
+    while True:
+        resto = dividendo % divisor
+        if (resto != 0):
+            dividendo = divisor
+            divisor = resto
+        else:
+            mdc = divisor
+            if (mdc == 1):
+                return a
+            else:
+                print("This key is not valid, because not correspond to '1 ≤ a ≤ m, gcd(a, m)=1'")
+                a = key_a_verification()
+                return a
+
+
+a = key_a_verification()
+
+# Key 'b' verification to the rule 1 ≤ b ≤ m
+def key_b_verfication():
+    b = int(input("Chave b: "))
+    if (b >= 1 and b <= len(alphabet)):
+        return b
+    else:
+        print("The key B needs to be 1 ≤ b ≤ m")
+        b = key_b_verfication()
+        return b
+
+b = key_b_verfication()
+
+
+# AFFINE CIPHER
+def affine_cipher(acm, a, b):
     result = ''
     for ac in acm:
-        d = a*contNumber(ac.upper(), al) + b
-        n = d / len(al)
-        c = d - len(al)*int(n)
-        c = al[c]
+        d = a*cont_number(ac.upper()) + b
+        n = d / len(alphabet)
+        c = d - len(alphabet)*int(n)
+        c = alphabet[c]
         result = result + c
     return result.lower()
 
-def contNumber(n, al):
+
+def cont_number(n):
 
     cont = 0
-    for a in al:
+    for a in alphabet:
         if n == a:
             return cont
         else:
             cont += 1
 
-print(affineCipher(mensage_clean, alphabet))
+
+print(affine_cipher(mensage_clean, a, b))
