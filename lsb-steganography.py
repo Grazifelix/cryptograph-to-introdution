8362054ab6dfeacf198cfca06428cffbcb442878
Grazi Felix <grazielafelix2001@gmail.com>
2022-08-02 21:13:36 -0300

    feat: complete implementation of the encode function

--- lsb-steganography.py
+++ lsb-steganography.py
@@ -6,18 +6,39 @@
 import numpy as np
 from PIL import Image
 
+
 # enconde fuction
-def Encode(src, message, dest):
+def encode(src, message, dest):
     img = Image.open(src, 'r')
     width, height = img.size
-    array = np.array(list(imag.getdata()))
+    array = np.array(list(img.getdata()))
 
+    n = 0
     if img.mode == 'RGB':
         n = 3
     elif img.mode == 'RGBA':
         n = 4
 
     total_pixels = array.size//n
+
+    message += '$t3g0'
+    b_message = ''.join([format(ord(i), "08b") for i in message])
+    req_pixels = len(b_message)
+
+    if req_pixels > total_pixels:
+        print("ERROR: need a larger file size")
+    else:
+        index = 0
+        for p in range(total_pixels):
+            for q in range(0, 3):
+                if index < req_pixels:
+                    array[p][q] = int(bin(array[p][q])[2:9] + b_message[index], 2)
+                    index += 1
+        array = array.reshape(height, width, n)
+        enc_img = Image.fromarray(array.astype('uint8'), img.mode)
+        enc_img.save(f"{dest}/enc_img.png")
+        print("Image Encoded Sucessfully")
+
 
 # LSB-STEGANOGRAPHY
 def main():
@@ -26,12 +47,13 @@ def main():
 
     if func == '1':
         message = input("Enter Message to Hide\n")
-        dest = ("Enter Destination Image Path\n")
+        dest = input("Enter Destination Image Path\n")
         print("Encoding...")
+        encode(src, message, dest)
 
     if func == '2':
         print("Decoding...")
 
+
 if __name__ == "__main__":
     main()
-
